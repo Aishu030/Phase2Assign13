b335628b09b7ae8e0f29a711c564d490e8a85f16
Aishu030 <aishuramesh031@gmail.com>
2023-08-29 16:22:13 +0530

    first commit

---
+++ Assignment13/Assignment13/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment13.Models
+{
+    [Table("Team")]
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TeamName { get; set; }
+        [Required]
+     
+        public int NOWC { get; set; }  
+    }
+}
